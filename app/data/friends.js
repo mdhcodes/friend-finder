4fd913879012f89669c6d1a5b52b8e355f31dbfa
Michelle Hartley <mdhcodes@gmail.com>
2017-04-27 01:01:09 -0400

    Update friends.js and add placeholder text to the default select option tag

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,22 @@
 var friendsArray = [
   {
     "name":"David",
-    "photo":"https://unsplash.it/400/400?image=0",
+    "photo":"https://unsplash.it/300/200/?random",
     "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
   },
   {
     "name":"Athena",
-    "photo":"https://unsplash.it/400/400?image=1",
+    "photo":"https://unsplash.it/300/200/?random",
     "scores":[ 1, 3, 5, 5, 4, 3, 1, 1, 2, 2 ]
   },
   {
     "name":"John",
-    "photo":"https://unsplash.it/400/400?image=2",
+    "photo":"https://unsplash.it/300/200/?random",
     "scores":[ 4, 4, 3, 5, 1, 1, 2, 5, 4, 1 ]
   },
   {
     "name":"Shirley",
-    "photo":"https://unsplash.it/400/400?image=3",
+    "photo":"https://unsplash.it/300/200/?random",
     "scores":[ 4, 4, 3, 2, 1, 1, 1, 5, 3, 1]
   },
 ];
